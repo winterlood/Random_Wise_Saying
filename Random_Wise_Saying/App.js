3543e36f3b1a4bd5626ddace1971da84eb8521a2
이종원 <jklh0202@naver.com>
2020-12-10 16:51:06 +0900

    [FEAT] : INITIAL

---
+++ Random_Wise_Saying/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>dd</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ Random_Wise_Saying/android/app/src/main/java/com/random_wise_saying/MainActivity.java
@@ -0,0 +1,15 @@
+package com.random_wise_saying;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Random_Wise_Saying";
+  }
+}
