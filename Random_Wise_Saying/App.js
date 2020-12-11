9a33482461a1c74064978bf2b42b17a0f4cbbac6
winterlood <king199777@gmail.com>
2020-12-12 00:16:59 +0900

    [FEAT] : 앱완성 및 출시준비1
    앞으로 필요한것
    - 7인치 스크린샷 2개
    - 10인치 스크린샷 2개
    - 앱 번들

--- Random_Wise_Saying/App.js
+++ Random_Wise_Saying/App.js
@@ -1,10 +1,14 @@
 /* eslint-disable prettier/prettier */
 
 import React, {useEffect, useState} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 // import BodyComponent from './src/BodyComponent/BodyComponent';
 import MainComponent from './src/MainComponent/MainComponent';
 const App = () => {
+  setTimeout(() => {
+    SplashScreen.hide();
+  }, 1000);
   return <MainComponent />;
 };
 
